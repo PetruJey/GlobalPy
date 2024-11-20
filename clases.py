314a39489ccf33596e5cb7ffdab0848fd305e8c2
PabloAVivas <pablovivas.mdz@gmail.com>
2024-11-20 17:46:12 -0300

    Add files via upload

--- clases.py
+++ clases.py
@@ -1,5 +1,5 @@
 import random
-class Detector:
+class Detector: # Clase encargada de detectar mutaciones
     SaludarD= "Bienvenido al Detector de Mutaciones"
     def __init__(self, adn_list):
         try:
@@ -9,7 +9,7 @@ class Detector:
             self.mutacion_vertical = 0
             self.mutacion_diagonal = 0
             print("Analizando ADN...")
-            self.analizar_mutaciones()  # Llama automáticamente a los métodos de análisis
+            self.analizar_mutaciones()  # Llama a los métodos del análisis
             self.resumen()
         except IndexError:
             print("Primero debe ingresar el ADN")
@@ -17,16 +17,16 @@ class Detector:
     def detectar_mutantes_horizontal(self):
         for i in range(len(self.adn_list)):
             cadena = self.adn_list[i]
-            for j in range(len(cadena) - 3):  # Verificar hasta 4 caracteres antes del final
+            for j in range(len(cadena) - 3):  # Verificar 4 caracteres antes del final
                 if cadena[j] == cadena[j+1] == cadena[j+2] == cadena[j+3]:
                     self.mutacion_horizontal += 1
                     self.mutacion = True
                     print(f"Mutación horizontal encontrada en la fila {i} posición {j}-{j+3}")
-                    break  # Salir al encontrar la primera mutación en la fila
+                    break  # Sale si encuentra la primera mutación de la fila
 
     def detectar_mutantes_vertical(self):
         for j in range(len(self.adn_list[0])):
-            for i in range(len(self.adn_list) - 3):  # Verificar hasta 4 filas antes del final
+            for i in range(len(self.adn_list) - 3):  # Verificar 4 filas antes del final
                 if (self.adn_list[i][j] == self.adn_list[i+1][j] == self.adn_list[i+2][j] == self.adn_list[i+3][j]):
                     self.mutacion_vertical += 1
                     self.mutacion = True
@@ -35,7 +35,7 @@ class Detector:
 
     def detectar_mutantes_diagonal(self):
         for i in range(len(self.adn_list) - 3):
-            for j in range(len(self.adn_list[0]) - 3):  # Verificar hasta 4 columnas antes del final
+            for j in range(len(self.adn_list[0]) - 3):  # Verificar 4 columnas antes del final
                 if (self.adn_list[i][j] == self.adn_list[i+1][j+1] == self.adn_list[i+2][j+2] == self.adn_list[i+3][j+3]):
                     self.mutacion_diagonal += 1
                     self.mutacion = True
@@ -44,7 +44,7 @@ class Detector:
 
     def detectar_mutantes_diagonal_inverso(self):
         for i in range(len(self.adn_list) - 3):
-            for j in range(3, len(self.adn_list[0])):  # Comenzar desde la columna 3 hasta el final
+            for j in range(3, len(self.adn_list[0])):  # Comienza desde la columna 3 hasta el final
                 if (self.adn_list[i][j] == self.adn_list[i+1][j-1] == self.adn_list[i+2][j-2] == self.adn_list[i+3][j-3]):
                     self.mutacion_diagonal += 1
                     self.mutacion = True
@@ -63,9 +63,8 @@ class Detector:
         print(f"Número de mutaciones horizontales: {self.mutacion_horizontal}")
         print(f"Número de mutaciones verticales: {self.mutacion_vertical}")
         print(f"Número de mutaciones en diagonales: {self.mutacion_diagonal}")
-        input("Presione Enter para continuar...")
 
-class Mutador:
+class Mutador: # Clase encargada de ver que tipo de mutacion quiere agregar
     __Saludar="Bienvenido al Mutador de ADN"
     def __init__(self, adn_list:list):
         self.base_nitrogenada = ("A", "G", "T", "C")
@@ -89,7 +88,7 @@ class Mutador:
                 elif muto == "2":
                     return 2  # Retorna 2 al elegir virus
                 elif muto == "0":
-                    print("Saliendo del programa...")
+                    print("Volviendo al programa principal...")
                     return 0  # Retorna 0 si quiere volver al menú principal
                 else:
                     print("Elija una opción en pantalla.")
@@ -101,7 +100,7 @@ class Mutador:
 
 
 
-class Radiacion(Mutador):
+class Radiacion(Mutador): # Clase encargada de añadir mutaciones horizontales y verticales
     def __init__(self, adn_list):
         super().__init__(adn_list)
         self.radiaciones= False
@@ -114,12 +113,12 @@ class Radiacion(Mutador):
           {adn_list[2]}
           {adn_list[3]}
           {adn_list[4]}
-          {adn_list[5]}""")  # Muestra el ADN modificado
+          {adn_list[5]}""")  # Muestra el adn modificado
         except ValueError and IndexError:
             print("Primero debe ingresar el ADN")
 
     def crear_mutantes_horizontal(self):
-        for posicion_vertical in range(len(self.adn_list)):
+        for posicion_vertical in range(len(self.adn_list)): # Si encuentra posibles mutaciones las genera
             cadena = self.adn_list[posicion_vertical]
             for posicion_horizontal in range(len(cadena) - 3):
                 if cadena[posicion_horizontal] == cadena[posicion_horizontal+1] == cadena[posicion_horizontal+2]:
@@ -129,7 +128,7 @@ class Radiacion(Mutador):
             if cadena[posicion_horizontal+3] == cadena[posicion_horizontal+2] == cadena[posicion_horizontal+1]:
                     cadena[posicion_horizontal] = cadena[posicion_horizontal+3]
                     self.radiaciones= True
-        if self.radiaciones == False:
+        if self.radiaciones == False: # Si no encuentra posibles mutaciones genera una aleatorea
             posicion_vertical = random.randint(0, len(self.adn_list))
             posicion_horizontal = random.randint(0, len(self.adn_list[0]) - 3)
             base=random.choice(self.base_nitrogenada)
@@ -137,7 +136,7 @@ class Radiacion(Mutador):
                 self.adn_list[posicion_vertical][posicion_horizontal] = base
             
     def crear_mutantes_vertical(self):
-        for posicion_horizontal in range(len(self.adn_list[0])):
+        for posicion_horizontal in range(len(self.adn_list[0])): # Si encuentra posibles mutaciones las genera
             for posicion_vertical in range(len(self.adn_list) - 3):
                 if (self.adn_list[posicion_vertical][posicion_horizontal] == self.adn_list[posicion_vertical+1][posicion_horizontal] ==self.adn_list[posicion_vertical+2][posicion_horizontal]):
                     self.adn_list[posicion_vertical+3][posicion_horizontal] = self.adn_list[posicion_vertical][posicion_horizontal]
@@ -146,7 +145,7 @@ class Radiacion(Mutador):
             if (self.adn_list[posicion_vertical+3][posicion_horizontal] == self.adn_list[posicion_vertical+2][posicion_horizontal] ==self.adn_list[posicion_vertical+1][posicion_horizontal]):
                 self.adn_list[posicion_vertical][posicion_horizontal] = self.adn_list[posicion_vertical+3][posicion_horizontal]
                 self.radiaciones= True
-        if self.radiaciones == False:
+        if self.radiaciones == False: # Si no encuentra posibles mutaciones genera una aleatorea
             posicion_vertical = random.randint(0, len(self.adn_list) - 3)
             posicion_horizontal = random.randint(0, len(self.adn_list[0]))
             base=random.choice(self.base_nitrogenada)
@@ -173,13 +172,13 @@ class Virus(Mutador):
           {adn_list[2]}
           {adn_list[3]}
           {adn_list[4]}
-          {adn_list[5]}""")  # Muestra el ADN modificado
+          {adn_list[5]}""")  # Muestra el adn modificado
         except IndexError and ValueError:
             print("Primero debe ingresar el ADN")
      
     def crear_mutantes_diagonal(self):
-        for self.posicion_vertical in range(len(self.adn_list) - 3):
-            for self.posicion_horizontal in range(len(self.adn_list[0]) - 3):  # Verificar hasta 4 columnas antes del final
+        for self.posicion_vertical in range(len(self.adn_list) - 3): # Si encuentra posibles mutaciones las genera
+            for self.posicion_horizontal in range(len(self.adn_list[0]) - 3):
                 if (self.adn_list[self.posicion_vertical][self.posicion_horizontal] == self.adn_list[self.posicion_vertical+1][self.posicion_horizontal+1] == self.adn_list[self.posicion_vertical+2][self.posicion_horizontal+2]):
                     self.adn_list[self.posicion_vertical+3][self.posicion_horizontal+3] = self.adn_list[self.posicion_vertical][self.posicion_horizontal]
                     self.viruses= True
@@ -187,18 +186,18 @@ class Virus(Mutador):
             if (self.adn_list[self.posicion_vertical+3][self.posicion_horizontal+3] == self.adn_list[self.posicion_vertical+2][self.posicion_horizontal+2] == self.adn_list[self.posicion_vertical+1][self.posicion_horizontal+1]):
                     self.adn_list[self.posicion_vertical][self.posicion_horizontal] = self.adn_list[self.posicion_vertical+3][self.posicion_horizontal+3]
                     self.viruses= True
-        if self.viruses == False:
+        if self.viruses == False: # Si no encuentra posibles mutaciones genera una aleatorea
             posicion_vertical = random.randint(0, len(self.adn_list) - 4)
             posicion_horizontal = random.randint(0, len(self.adn_list[0]) - 4)
             base=random.choice(self.base_nitrogenada)
-            for i in range(4):  # Se requieren 4 cambios en diagonal
+            for i in range(4):  
                 if (posicion_vertical + i < len(self.adn_list) and 
                         posicion_horizontal + i < len(self.adn_list[0])):
                     self.adn_list[posicion_vertical + i][posicion_horizontal + i] = base
 
     def crear_mutantes_diagonal_inverso(self):
-        for self.posicion_vertical in range(len(self.adn_list) - 3):
-            for self.posicion_horizontal in range(3, len(self.adn_list[0])):  # Comenzar desde la columna 3 hasta el final
+        for self.posicion_vertical in range(len(self.adn_list) - 3): # Si encuentra posibles mutaciones las genera
+            for self.posicion_horizontal in range(3, len(self.adn_list[0])):  
                 if (self.adn_list[self.posicion_vertical][self.posicion_horizontal] == self.adn_list[self.posicion_vertical+1][self.posicion_horizontal-1] == self.adn_list[self.posicion_vertical+2][self.posicion_horizontal-2]):
                     self.adn_list[self.posicion_vertical+3][self.posicion_horizontal-3] = self.adn_list[self.posicion_vertical][self.posicion_horizontal]
                     self.viruses= True
@@ -206,11 +205,11 @@ class Virus(Mutador):
             if (self.adn_list[self.posicion_vertical+3][self.posicion_horizontal-3] == self.adn_list[self.posicion_vertical+2][self.posicion_horizontal-2] == self.adn_list[self.posicion_vertical+1][self.posicion_horizontal-1]):
                     self.adn_list[self.posicion_vertical][self.posicion_horizontal] = self.adn_list[self.posicion_vertical+3][self.posicion_horizontal-3]
                     self.viruses= True
-        if self.viruses == False:
+        if self.viruses == False: # Si no encuentra posibles mutaciones genera una aleatorea
             posicion_vertical = random.randint(3, len(self.adn_list) - 1)
             posicion_horizontal = random.randint(3, len(self.adn_list[0]) - 1)
             base=random.choice(self.base_nitrogenada)
-            for i in range(4):  # Se requieren 4 cambios en diagonal
+            for i in range(4): 
                 if (posicion_vertical - i >= 0 and 
                         posicion_horizontal - i >= 0):
                     self.adn_list[posicion_vertical - i][posicion_horizontal - i] = base
@@ -220,46 +219,45 @@ class Virus(Mutador):
         self.crear_mutantes_diagonal_inverso()
         print("Mutaciones completadas.")
 
-class Sanador:
+class Sanador: # Clase encargada de Sanar el adn
     SaludarS= "Bienvenido al Sanador de Mutaciones"
     def __init__(self, adn_list):
         try:
             self.base_nitrogenada = ("A", "G", "T", "C")
             self.adn_list = adn_list
             self.mutacion = False
-            self.analizar_mutaciones()  # Llama automáticamente a los métodos de análisis
+            self.analizar_mutaciones()  # Llama a los métodos de análisis
         except IndexError:
             print("Primero debe ingresar el ADN")
 
     def detectar_mutantes_horizontal(self):
         for i in range(len(self.adn_list)):
             cadena = self.adn_list[i]
-            for j in range(len(cadena) - 3):  # Verificar hasta 4 caracteres antes del final
+            for j in range(len(cadena) - 3):  # Verificar 4 caracteres antes del final
                 if cadena[j] == cadena[j+1] == cadena[j+2] == cadena[j+3]:
                     self.mutacion = True
-                    break  # Salir al encontrar la primera mutación en la fila
+                    break  # Sale si encuentra la primera mutación en la fila
 
     def detectar_mutantes_vertical(self):
         for j in range(len(self.adn_list[0])):
-            for i in range(len(self.adn_list) - 3):  # Verificar hasta 4 filas antes del final
+            for i in range(len(self.adn_list) - 3):  # Verificar 4 filas antes del final
                 if (self.adn_list[i][j] == self.adn_list[i+1][j] == self.adn_list[i+2][j] == self.adn_list[i+3][j]):
                     self.mutacion = True
                     break
 
     def detectar_mutantes_diagonal(self):
         for i in range(len(self.adn_list) - 3):
-            for j in range(len(self.adn_list[0]) - 3):  # Verificar hasta 4 columnas antes del final
+            for j in range(len(self.adn_list[0]) - 3):  # Verificar 4 columnas antes del final
                 if (self.adn_list[i][j] == self.adn_list[i+1][j+1] == self.adn_list[i+2][j+2] == self.adn_list[i+3][j+3]):
                     self.mutacion = True
                     break
 
     def detectar_mutantes_diagonal_inverso(self):
         for i in range(len(self.adn_list) - 3):
-            for j in range(3, len(self.adn_list[0])):  # Comenzar desde la columna 3 hasta el final
+            for j in range(3, len(self.adn_list[0])):  # Comienza desde la columna 3 hasta el final
                 if (self.adn_list[i][j] == self.adn_list[i+1][j-1] == self.adn_list[i+2][j-2] == self.adn_list[i+3][j-3]):
                     self.mutacion = True
                     break
-
 
     def generar_lista_random(self):
         # Genera una matriz 6x6 con letras aleatorias de self.base_nitrogenada
@@ -267,7 +265,7 @@ class Sanador:
 
     def analizar_mutaciones(self):
         while True:
-            self.mutacion = False  # Reinicia el estado de mutación
+            self.mutacion = False  # Reinicia mutación
             self.detectar_mutantes_horizontal()
             self.detectar_mutantes_vertical()
             self.detectar_mutantes_diagonal()
@@ -276,7 +274,7 @@ class Sanador:
                 self.generar_lista_random()  # Genera una nueva lista si hay mutación
             else:
                 break  # Sale del bucle si no hay mutación
-        # Imprime el ADN final si no se detectaron mutaciones
+        # Imprime el adn nuevo si no se detectaron mutaciones
         print(f"""ADN:
       {self.adn_list[0]}
       {self.adn_list[1]}
--- funciones.py
+++ funciones.py
@@ -13,11 +13,12 @@ def guia():
     example = str(input("Presione una Enter para continuar..."))
     
 def verificar_adn(value):
+    #Se encarga de verificar si los datos ingresados por el usuario son correctos
     bases_nitrogenadas = ("A", "G", "T", "C") 
     if len(value) != 6:  
         print("Error: La fila debe contener 6 bases nitrogenadas.")
         return False
-
+    #Se encarga de verificar que se ingresen 6 datos
     for base in value:  
         if base not in bases_nitrogenadas:
             print("Error: Tiene que ingresar únicamente bases nitrogenadas.")
@@ -26,6 +27,7 @@ def verificar_adn(value):
     return True 
     
 def ingresar_adn(adn):
+    #Se encarga de tomar los datos al usuario para crear una lista
     i = 0
     posiciones_list = ("la primera", "la segunda", "la tercera", "la cuarta", "la quinta", "la sexta")
     if not adn:
@@ -33,17 +35,18 @@ def ingresar_adn(adn):
             value = input(f"Ingrese {posiciones_list[i]} fila de su ADN: ")
             value = value.upper()
             if verificar_adn(value):
-                adn.append(list(value))  # Guardar como lista de caracteres
+                adn.append(list(value))  # Guardar como lista de listas de caracteres
                 i += 1
             else:
                 print("Fila inválida, Ingrese de nuevo.")
     else:
+        #Pregunta al usuario si quiere borrar el adn actual guardado y crear uno nuevo o conservarlo
         example = str(input("Ya existe un ADN guardado, desea borrarlo?\n1 - Si\n2 - No\n"))
         if example == "1":
             adn.clear()
         else:
             print("Volviendo al Menu...")
-    try:
+    try: #Muestra el adn complet como si fuera una matriz salvo que no exista ese adn
         print(f"""ADN:
           {adn[0]}
           {adn[1]}
